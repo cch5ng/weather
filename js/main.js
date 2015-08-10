c16e6f95d8bbf85f79ab5cc91a90fb51c6e5e6fa
cch5ng <cch5ng@gmail.com>
2015-08-10 13:45:20 -0700

    fixed issue with getting lat/long from geolocation api. successfully retrieving city data from wunderg

--- js/main.js
+++ js/main.js
@@ -7,19 +7,16 @@ function getWeather() {
   
   function success(data) {
 	var AK = '9e8a6bd909702d86';
-    var latitude, longitude;
 
-	navigator.geolocation.getCurrentPosition(function(position) {
-		latitude = position.coords.latitude;
-        console.log('latitude: ' + latitude);
-		longitude = position.coords.longitude;
+	var latitude = data.coords.latitude;
+    console.log('latitude: ' + latitude);
+    var longitude = data.coords.longitude;
+    console.log('longitude: ' + longitude);
       
-	});
-
     var url = 'http://api.wunderground.com/api/' + AK + '/geolookup/q/' + latitude + ',' + longitude + '.json';
       
     $.getJSON(url, function(data) {
-        console.log('gets here too');
+        //console.log('gets here too');
         console.log('data: ' + data);
 	    var city = data.location.city;
         console.log(city);
@@ -37,13 +34,6 @@ function getWeather() {
 }
 
 document.addEventListener('DOMContentLoaded', function(event) {
-  console.log('gets here');
+  //console.log('gets here');
   getWeather();
 });
-
-
-// if ('geolocation' in navigator) {
-
-// } else {
-// 	var msg = 'Your browser does not support geolocation. Please update your browser to a current version. Opera Mini browser is not supported by this application. ';
-// }
